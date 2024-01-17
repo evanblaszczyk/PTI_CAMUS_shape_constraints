Fix gradient backprop though encoder, modify loss formula and clarify code

src/models/autoencoder.py
@@ -1,178 +1,166 @@
-from typing import Tuple
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch import Tensor
+class DoubleConv(nn.Module):
 
-
-class Auto_Encoder(nn.Module):
-    """Architecture based on U-Net: Convolutional Networks for Biomedical Image Segmentation.
-
-    Retrieved from:
-    https://github.com/vitalab/vital/blob/dev/vital/models/segmentation/unet.py
-
-    References:
-    - Paper that introduced the U-Net model: https://arxiv.org/abs/1505.04597
-    """
-
-    def __init__(
-        self,
-        input_shape: Tuple[int, ...],
-        output_shape: Tuple[int, ...],
-        init_channels: int = 32,
-        use_batchnorm: bool = True,
-        bilinear: bool = False,
-        dropout: float = 0.0,
-    ):
-        """Initializes class instance.
-
-        Args:
-            input_shape: (in_channels, H, W), Shape of the input images.
-            output_shape: (num_classes, H, W), Shape of the output segmentation map.
-            init_channels: Number of output feature maps from the first layer, used to compute the number of feature
-                maps in following layers.
-            use_batchnorm: Whether to use batch normalization between the convolution and activation layers in the
-                convolutional blocks.
-            bilinear: Whether to use bilinear interpolation or transposed convolutions for upsampling.
-            dropout: Probability of an element to be zeroed (e.g. 0 means no dropout).
-        """
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        in_channels = input_shape[0]
-        out_channels = output_shape[0]
-
-        self.layer1 = _DoubleConv(in_channels, init_channels // 2, dropout / 2, use_batchnorm)
-        self.layer2 = _Down(init_channels // 2, init_channels, dropout, use_batchnorm)
-        self.layer3 = _Down(init_channels, init_channels * 2, dropout, use_batchnorm)
-        self.layer4 = _Down(init_channels * 2, init_channels * 4, dropout, use_batchnorm)
-        self.layer5 = _Down(init_channels * 4, init_channels * 8, dropout, use_batchnorm)
-
-        self.layer6 = _Up(
-            init_channels * 8, init_channels * 4, dropout, use_batchnorm, bilinear=bilinear
-        )
-        self.layer7 = _Up(
-            init_channels * 4, init_channels * 2, dropout, use_batchnorm, bilinear=bilinear
-        )
-        self.layer8 = _Up(
-            init_channels * 2, init_channels, dropout, use_batchnorm, bilinear=bilinear
-        )
-        self.layer9 = _Up(init_channels, init_channels // 2, 0, use_batchnorm, bilinear=bilinear)
-
-        self.layer10 = nn.Conv2d(init_channels // 2, out_channels, kernel_size=1)
-
-        # Use Xavier initialisation for weights
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-                nn.init.xavier_uniform_(m.weight)
-
-    def forward(self, x: Tensor) -> Tensor:
-        """Defines the computation performed at every call.
-
-        Args:
-            x: (N, ``in_channels``, H, W), Input image to segment.
-
-        Returns:
-            (N, ``out_channels``, H, W), Raw, unnormalized scores for each class in the input's segmentation.
-        """
-        x1 = self.layer1(x)
-        x2 = self.layer2(x1)
-        x3 = self.layer3(x2)
-        x4 = self.layer4(x3)
-        x5 = self.layer5(x4)
-
-        out = self.layer6(x5, x4)
-        out = self.layer7(out, x3)
-        out = self.layer8(out, x2)
-        out = self.layer9(out, x1)
-
-        return self.layer10(out)
-
-
-class _DoubleConv(nn.Module):
-    """Double Convolution and BN and ReLU.
-
-    (3x3 conv -> BN -> ReLU) ** 2
-    """
-
-    def __init__(self, in_ch: int, out_ch: int, dropout_prob: float, use_batchnorm: bool):
-        super().__init__()
-        if use_batchnorm:
-            self.net = nn.Sequential(
-                nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
-                nn.BatchNorm2d(out_ch),
-                nn.ReLU(inplace=True),
-                nn.Dropout(p=dropout_prob),
-                nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
-                nn.BatchNorm2d(out_ch),
-                nn.ReLU(inplace=True),
-                nn.Dropout(p=dropout_prob),
-            )
-        else:
-            self.net = nn.Sequential(
-                nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Dropout(p=dropout_prob),
-                nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Dropout(p=dropout_prob),
-            )
-
-    def forward(self, x: Tensor) -> Tensor:
-        return self.net(x)
-
-
-class _Down(nn.Module):
-    """Combination of MaxPool2d and DoubleConv in series."""
-
-    def __init__(self, in_ch: int, out_ch: int, dropout_prob: float, use_batchnorm: bool):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            _DoubleConv(in_ch, out_ch, dropout_prob, use_batchnorm),
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
         )
 
-    def forward(self, x: Tensor) -> Tensor:  # noqa: D102
-        return self.net(x)
+    def forward(self, x):
+        return self.double_conv(x)
 
+class Down(nn.Module):
 
-class _Up(nn.Module):
-    """Upsampling (by either bilinear interpolation or transpose convolutions).
-
-    followed by concatenation of feature map from contracting path, followed by double 3x3
-    convolution.
-    """
-
-    def __init__(
-        self, in_ch, out_ch: int, dropout_prob: float, use_batchnorm: bool, bilinear: bool = False
-    ):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.upsample = None
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels),
+            #nn.Dropout2d(p=0.2)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+class Up(nn.Module):
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to modify the number of channels
         if bilinear:
-            self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+            self.up = nn.Sequential(
+                nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+                DoubleConv(in_channels, out_channels),
+                #nn.Dropout2d(p=0.2)
+            )
         else:
-            self.upsample = nn.ConvTranspose2d(in_ch, in_ch // 2, kernel_size=2, stride=2)
+            self.up = nn.Sequential(
+                nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+                #nn.Dropout2d(p=0.2)
+            )
 
-        self.conv = _DoubleConv(in_ch // 2, out_ch, dropout_prob, use_batchnorm)
+    def forward(self, x):
+        return self.up(x)
 
-    def forward(self, x: Tensor, connected_encoder_features: Tensor) -> Tensor:
-        x = self.upsample(x)
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
-        # Pad ``x`` to the size of ``connected_encoder_features``
-        diff_h = connected_encoder_features.shape[2] - x.shape[2]
-        diff_w = connected_encoder_features.shape[3] - x.shape[3]
-
-        x = F.pad(x, [diff_w // 2, diff_w - diff_w // 2, diff_h // 2, diff_h - diff_h // 2])
-
-        # Concatenate along the channels axis
-        #x = torch.cat([connected_encoder_features, x], dim=1)
-
+    def forward(self, x):
         return self.conv(x)
 
+class CompressConv(nn.Module):
+    def __init__(self, in_channels, compress_channel):
+        super(CompressConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, compress_channel, kernel_size=1)
 
-if __name__ == "__main__":
-    from torchinfo import summary
+    def forward(self, x):
+        return self.conv(x)
 
-    input_shape = (1, 64, 64)
-    output_shape = (4, 64, 64)
-    unet = UNet(input_shape, output_shape)
-    summary(unet, (1, *input_shape), device="cpu")
+class DeCompressConv(nn.Module):
+    def __init__(self, compress_channel, out_channels):
+        super(DeCompressConv, self).__init__()
+        self.conv = nn.Conv2d(compress_channel, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+        
+class AE_Conv(nn.Module):
+    def __init__(self, in_channels, out_channels, init_channel, compress_channel, bilinear=True):
+        super(AE_Conv, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.init_channel = init_channel
+        self.compress_channel = compress_channel
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(in_channels, init_channel)
+        self.down1 = Down(init_channel, init_channel*2)
+        self.down2 = Down(init_channel*2, init_channel*4)
+        self.down3 = Down(init_channel*4, init_channel*8)
+        self.down4 = Down(init_channel*8, init_channel*16)
+        self.compress = CompressConv(init_channel*16, compress_channel)
+        self.decompress = DeCompressConv(compress_channel, init_channel*16)
+        self.up4 = Up(init_channel*16, init_channel*8, bilinear)
+        self.up3 = Up(init_channel*8, init_channel*4, bilinear)
+        self.up2 = Up(init_channel*4, init_channel*2, bilinear)
+        self.up1 = Up(init_channel*2, init_channel, bilinear)
+        self.outc = OutConv(init_channel, out_channels)
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.inc(x)
+        x = self.down1(x)
+        x = self.down2(x)
+        x = self.down3(x)
+        x = self.down4(x)
+        x = self.compress(x)
+        x = self.decompress(x)
+        x = self.up4(x)
+        x = self.up3(x)
+        x = self.up2(x)
+        x = self.up1(x)
+        x = self.outc(x)
+        x = torch.sigmoid(x)
+        return x
+
+    def _initialize_weights(self):
+       for m in self.modules():
+           if isinstance(m, nn.Conv2d):
+               nn.init.kaiming_normal_(m.weight)
+               if m.bias is not None:
+                   nn.init.zeros_(m.bias)
+           elif isinstance(m, nn.BatchNorm2d):
+               nn.init.constant_(m.weight, 1)
+               nn.init.constant_(m.bias, 0)
+               
+class Encoder(nn.Module):
+    def __init__(self, in_channels, init_channel, compress_channel, bilinear=True):
+        super(Encoder, self).__init__()
+        self.in_channels = in_channels
+        self.init_channel = init_channel
+        self.compress_channel = compress_channel
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(in_channels, init_channel)
+        self.down1 = Down(init_channel, init_channel*2)
+        self.down2 = Down(init_channel*2, init_channel*4)
+        self.down3 = Down(init_channel*4, init_channel*8)
+        self.down4 = Down(init_channel*8, init_channel*16)
+        self.compress = CompressConv(init_channel*16, compress_channel)
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.inc(x)
+        x = self.down1(x)
+        x = self.down2(x)
+        x = self.down3(x)
+        x = self.down4(x)
+        x = self.compress(x)
+        return x
+        
+    def _initialize_weights(self):
+       for m in self.modules():
+           if isinstance(m, nn.Conv2d):
+               nn.init.kaiming_normal_(m.weight)
+               if m.bias is not None:
+                   nn.init.zeros_(m.bias)
+           elif isinstance(m, nn.BatchNorm2d):
+               nn.init.constant_(m.weight, 1)
+               nn.init.constant_(m.bias, 0)